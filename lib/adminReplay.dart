6af1e482ce68fa5aa8dac791e3cd39fd8533edd6
saleelmhd <mohammedsaleeledk@gmail.com>
2025-11-13 22:44:17 +0530

    13/nov/2025

--- lib/adminReplay.dart
+++ lib/adminReplay.dart
@@ -17,105 +17,107 @@ class _UserChatScreenState extends State<UserChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Chat with ${widget.sender}",
-          style: TextStyle(fontSize: 14),
+    return SafeArea(
+      child: Scaffold(extendBody: true,
+        appBar: AppBar(
+          title: Text(
+            "Chat with ${widget.sender}",
+            style: TextStyle(fontSize: 14),
+          ),
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
         ),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: Consumer<ChatProvider>(
-        builder: (context, chatProvider, child) {
-          var messages =
-              chatProvider.getMessagesBetween(widget.sender, widget.recipient);
-
-          return Column(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView.builder(
-                    itemCount: messages.length,
-                    itemBuilder: (context, index) {
-                      var msg = messages[index];
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          title: Text("${msg.sender}: ${msg.message}"),
-                          subtitle: Text(msg.timestamp.toString()),
-                          tileColor: msg.sender == widget.sender
-                              ? Colors.blue[100]
-                              : Colors.grey[200],
-                        ),
-                      );
-                    },
+        body: Consumer<ChatProvider>(
+          builder: (context, chatProvider, child) {
+            var messages =
+                chatProvider.getMessagesBetween(widget.sender, widget.recipient);
+      
+            return Column(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView.builder(
+                      itemCount: messages.length,
+                      itemBuilder: (context, index) {
+                        var msg = messages[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            title: Text("${msg.sender}: ${msg.message}"),
+                            subtitle: Text(msg.timestamp.toString()),
+                            tileColor: msg.sender == widget.sender
+                                ? Colors.blue[100]
+                                : Colors.grey[200],
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _replyController,
-                        decoration: InputDecoration(
-                          hintText: "Reply to ${widget.sender}...",
-                          hintStyle: TextStyle(fontSize: 12),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide:
-                                BorderSide(color: Colors.blue, width: 2),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide:
-                                BorderSide(color: Colors.grey, width: 1.5),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide:
-                                BorderSide(color: Colors.blue, width: 2),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(expands: true,
+                          controller: _replyController,
+                          decoration: InputDecoration(
+                            hintText: "Reply to ${widget.sender}...",
+                            hintStyle: TextStyle(fontSize: 12),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide:
+                                  BorderSide(color: Colors.blue, width: 2),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 1.5),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide:
+                                  BorderSide(color: Colors.blue, width: 2),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.indigo,
+                      SizedBox(
+                        width: 10,
                       ),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.send,
-                          color: Colors.white,
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.indigo,
                         ),
-                        onPressed: () {
-                          if (_replyController.text.isNotEmpty) {
-                            Provider.of<ChatProvider>(context, listen: false)
-                                .sendMessage(
-                              widget.recipient,
-                              widget.sender,
-                              _replyController.text,
-                            );
-                            _replyController.clear();
-                          }
-                        },
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.send,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            if (_replyController.text.isNotEmpty) {
+                              Provider.of<ChatProvider>(context, listen: false)
+                                  .sendMessage(
+                                widget.recipient,
+                                widget.sender,
+                                _replyController.text,
+                              );
+                              _replyController.clear();
+                            }
+                          },
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          );
-        },
+              ],
+            );
+          },
+        ),
       ),
     );
   }
